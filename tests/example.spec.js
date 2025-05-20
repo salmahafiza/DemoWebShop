bf03d9cf8c590c5a01a83c710b744e95b12d8d3c
HARRIS\am16608 <amumtaz@cayenta.com>
2025-05-20 15:55:59 +0500

    updated main branch

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,8 +3,6 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
@@ -16,4 +14,9 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('1 has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
 });
