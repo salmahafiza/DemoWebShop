1625cfe2bb927bc56330d803e38109b951e2b7e8
TL\ghulasal <salma.2025pseb3@gmail.com>
2025-06-23 17:56:44 +0500

    TC_DASHBOARD_002B

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -14,7 +14,7 @@ test.beforeEach(async ({ page }) => {
     await dashboard.navigateToLoginPage();
 
 });
-test.only('TC_DASHBOARD_001 - Verify that user information is correctly showed on the dashboard page', async ({ page }) => {
+test('TC_DASHBOARD_001 - Verify that user information is correctly showed on the dashboard page', async ({ page }) => {
     await login.enterUsername(Users.username);
     await login.enterPassword(Users.password);
     await login.clickLoginButton();
@@ -22,7 +22,14 @@ test.only('TC_DASHBOARD_001 - Verify that user information is correctly showed o
     await dashboard.clickOnUserAccount();      
 
 });
-
+test('TC_DASHBOARD_002 - Verify that logout link is working properly', async ({ page }) => {
+    await login.enterUsername(Users.username);
+    await login.enterPassword(Users.password);
+    await login.clickLoginButton();
+    await dashboard.verifyUserInfoVisible();
+    await dashboard.verifyToLogout();
+    await expect(page).toHaveURL('https://demowebshop.tricentis.com/');
+});
 
 
 
