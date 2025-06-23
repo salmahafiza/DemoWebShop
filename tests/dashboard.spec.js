6040711a6c0153cd51fb477ff89b877c5f10cd81
TL\ghulasal <salma.2025pseb3@gmail.com>
2025-06-23 17:44:16 +0500

    TC_DASHBOARD_001B

--- tests/dashboard.spec.js
+++ tests/dashboard.spec.js
@@ -14,6 +14,15 @@ test.beforeEach(async ({ page }) => {
     await dashboard.navigateToLoginPage();
 
 });
+test.only('TC_DASHBOARD_001 - Verify that user information is correctly showed on the dashboard page', async ({ page }) => {
+    await login.enterUsername(Users.username);
+    await login.enterPassword(Users.password);
+    await login.clickLoginButton();
+    await dashboard.verifyUserInfoVisible();
+    await dashboard.clickOnUserAccount();      
+
+});
+
 
 
 
