6c0e32eeb7c285ffa7d461cec00eb33e63ef9834
Saniya Muskan <saniya.muskan@contour-software.com>
2025-06-26 17:58:59 +0500

    TC_PDP_001

---
+++ pages/PDP.js
@@ -0,0 +1,63 @@
+import { expect } from '@playwright/test';
+
+class PDP_Page {
+    constructor(page) {
+        this.page = page;
+        this.PDPproductName = page.locator('.product-name');
+        this.PDPproductPrice = page.locator("//span[@class='price-value-43']");
+        this.PLPtitle = page.getByRole("link", { name: "Smartphone", exact: true, });
+        this.PLPPrice = page.locator(".product-item").nth(0).locator("span.price.actual-price");
+
+        this.PLPproductPrice = page
+            .locator(".product-item")
+            .nth(0)
+            .locator("span.price.actual-price");
+        // this.addToCartButton = page.locator('#add-to-cart-button-4'); ("//p[@class='content']")
+        // this.addToWishlistButton = page.locator('.add-to-wishlist-button');
+        // this.productDescriptionTab = page.locator('#product-tabs .product-description-tab');
+        // this.productReviewsTab = page.locator('#product-tabs .product-reviews-tab');
+        // this.productReviewsCount = page.locator('.review-count');
+        // this.GoToElectronicCategory = page.locator('.Electronics');
+    }
+
+    async NavigateToDifferentCategoriesWithAssert(category) {
+        await this.page
+            .locator(`//li[@class='inactive']//a[normalize-space()='${category}']`)
+            .click();
+        await expect(
+            this.page.locator(`//h1[normalize-space()='${category}']`)
+        ).toHaveText(category);
+    }
+
+    async NavigateToSubcategory(subcategory) {
+        const heading = await this.page.getByRole("heading", { name: subcategory }).first();
+        await heading.locator("a").click();
+    }
+
+    async getPLPProductDetails() {
+        const PLPPrice = (await this.PLPPrice.textContent()).trim();
+        const PLPTitle = (await this.PLPtitle.textContent()).trim();
+        console.log(`PLP Product Name: ${PLPTitle}`);
+        console.log(`PLP Product Price: ${PLPPrice}`);
+        return { plpProductPrice: PLPPrice, plpProductName: PLPTitle };
+    }
+
+    async NavigateToProductPDP(ProductName) {
+        await this.page.locator(`//a[normalize-space()='${ProductName}']`).click();
+        await expect(await this.page.locator(`//h1[normalize-space()='${ProductName}']`)).toBeVisible();
+    }
+
+    async VerifyandAssertPDPWithPLP(plpProductName, plpProductPrice) {
+        await expect(this.PDPproductName).toBeVisible();
+        await expect(this.PDPproductPrice).toBeVisible();
+        const pdpProductName = (await this.PDPproductName.textContent()).trim();
+        const pdpProductPrice = (await this.PDPproductPrice.textContent()).trim();
+        console.log(`PDP Product Name: ${pdpProductName}`);
+        console.log(`PDP Product Price: ${pdpProductPrice}`);
+        expect(pdpProductName).toBe(plpProductName);
+        expect(pdpProductPrice).toBe(plpProductPrice);
+    }
+
+}
+
+module.exports = { PDP_Page };
---
+++ tests/pdp.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+const { DashboardPage } = require('../pages/DashboardPage');
+const { PDP_Page } = require('../pages/PDP');
+
+let dashboard;
+let pdp;
+
+test.beforeEach(async ({ page }) => {
+    dashboard = new DashboardPage(page);
+    pdp = new PDP_Page(page);
+    await dashboard.accessApplication();
+});
+
+test('TC_PDP_001: Verify that product details are displayed correctly', async ({ page }) => {
+    await pdp.NavigateToDifferentCategoriesWithAssert('Electronics');
+    await pdp.NavigateToSubcategory('Cell phones');
+    const { plpProductPrice, plpProductName } = await pdp.getPLPProductDetails();
+    await pdp.NavigateToProductPDP('Smartphone');
+    await pdp.VerifyandAssertPDPWithPLP(plpProductName, plpProductPrice);
+});
