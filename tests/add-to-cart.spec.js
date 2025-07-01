8c394086db51cc86b1c669086a3e464e5dd16318
hme2025pseb3 <hme.2025pseb3@gmail.com>
2025-07-01 14:48:46 +0500

    TC_ShoppingCart_015

--- tests/add-to-cart.spec.js
+++ tests/add-to-cart.spec.js
@@ -106,5 +106,46 @@ test('TC_ShoppingCart_014: Verify Cart is Empty after the order is placed', asyn
     await checkout.emptyCart();
 });
 
+test('TC_ShoppingCart_015: Verify Cart is Empty after the order is placed', async () => {
+    await dashboard.navigateToLoginPage();
+    await login.enterUsername(Users.username);
+    await login.enterPassword(Users.password);
+    await login.clickLoginButton();
+    await checkout.searchTextBox('Smartphone');
+    await dashboard.clickOnSearchButton();
+    await checkout.clickOnProductName();
+    await checkout.clickOnAdtoCart();
+    await checkout.gotoShoppingCart();
+    //await checkout.gotoCart();
+    await checkout.assertShoppingCartPage();
+    await checkout.selectCountry('Pakistan');
+    await checkout.selectState('Other (Non US)');
+    await checkout.enterZipCode('74500');
+    await checkout.clickEstimateShipping();
+    await checkout.verifyShippingOptionsVisible();
+    await checkout.acceptTermsAndCondition();
+    await checkout.proceedToCheckOut();
+    await checkout.selectAddNewAddress();
+    await checkout.fillBillingAddress(billingAddressData);
+    await checkout.clickContinue();
+    await checkout.clickContinueShippingSave();
+    await checkout.selectShippingMethod('Ground (0.00)');  // Options: Ground, Next Day Air, Second Day Air
+    await checkout.clickContinueShippingMethod();
+    await checkout.selectPaymentMethod('Credit Card'); 
+    // Options: Cash On Delivery (COD) (7.00), Check / Money Order (5.00), Credit Card, Purchase Order
+    await checkout.clickContinuePaymentMethod();
+    await checkout.PaymentViaCreditCard(creditCardDetails);
+    await checkout.ContinueViaCard();
+    await checkout.confirm();
+    //await expect (page.locator("//h2[normalize-space()='Payment information']")).toBeVisible();
+    //await checkout.COD_confirmationText();
+    //await checkout. ContinuePayment();
+    await checkout.clickConfirmOrder();
+    await checkout.OrderConfirmationMsg();
+    await checkout.clickContinueLastBtn();
+    await checkout.gotoShoppingCart();
+    await checkout.emptyCart();
+});
+
 
 
