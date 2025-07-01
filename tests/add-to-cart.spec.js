de93b3d77ee1bfd4b224bd8e5d52705e9d61ee1c
hme2025pseb3 <hme.2025pseb3@gmail.com>
2025-07-01 14:36:57 +0500

    TC_ShoppingCart_014

--- tests/add-to-cart.spec.js
+++ tests/add-to-cart.spec.js
@@ -4,7 +4,7 @@ const { DashboardPage } = require('../pages/DashboardPage');
 const { ATC } = require('../pages/ATC');
 const { PDP } = require('../pages/PDP');
 const { Checkout } = require('../pages/Checkout');
-const { Users } = require('../test-data/Users');
+const { Users,billingAddressData, creditCardDetails, } = require('../test-data/Users');
 
 
 let login;
@@ -64,5 +64,47 @@ test('TC_ShoppingCart_013: Validate Read link to terms of service.', async () =>
     await checkout.gotoShoppingCart();
     await checkout.acceptTermsAndCondition();
 });
+
+test('TC_ShoppingCart_014: Verify Cart is Empty after the order is placed', async () => {
+    await dashboard.navigateToLoginPage();
+    await login.enterUsername(Users.username);
+    await login.enterPassword(Users.password);
+    await login.clickLoginButton();
+    await checkout.searchTextBox('Smartphone');
+    await dashboard.clickOnSearchButton();
+    await checkout.clickOnProductName();
+    await checkout.clickOnAdtoCart();
+    await checkout.gotoShoppingCart();
+    //await checkout.gotoCart();
+    await checkout.assertShoppingCartPage();
+    await checkout.selectCountry('Pakistan');
+    await checkout.selectState('Other (Non US)');
+    await checkout.enterZipCode('74500');
+    await checkout.clickEstimateShipping();
+    await checkout.verifyShippingOptionsVisible();
+    await checkout.acceptTermsAndCondition();
+    await checkout.proceedToCheckOut();
+    await checkout.selectAddNewAddress();
+    await checkout.fillBillingAddress(billingAddressData);
+    await checkout.clickContinue();
+    await checkout.clickContinueShippingSave();
+    await checkout.selectShippingMethod('Ground (0.00)');  // Options: Ground, Next Day Air, Second Day Air
+    await checkout.clickContinueShippingMethod();
+    await checkout.selectPaymentMethod('Credit Card'); 
+    // Options: Cash On Delivery (COD) (7.00), Check / Money Order (5.00), Credit Card, Purchase Order
+    await checkout.clickContinuePaymentMethod();
+    await checkout.PaymentViaCreditCard(creditCardDetails);
+    await checkout.ContinueViaCard();
+    await checkout.confirm();
+    //await expect (page.locator("//h2[normalize-space()='Payment information']")).toBeVisible();
+    //await checkout.COD_confirmationText();
+    //await checkout. ContinuePayment();
+    await checkout.clickConfirmOrder();
+    await checkout.OrderConfirmationMsg();
+    await checkout.clickContinueLastBtn();
+    await checkout.gotoShoppingCart();
+    await checkout.emptyCart();
+});
+
 
 
